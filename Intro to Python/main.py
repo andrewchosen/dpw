cb151049f14eed1dc962303838e03d43d7afc611
andrewchosen <andrewchosen@fullsail.edu>
2015-09-29 15:45:20 -0700

    Intro to Python
    Updates/notes from “Comments, Variables and Printing in Python” video.

---
+++ Intro to Python/main.py
@@ -0,0 +1,16 @@
+#one line comment
+'''
+Doc strings
+
+'''
+
+first_name = "Kermit"
+last_name = "De Frog"
+
+# response = raw_input("Enter your name   ")
+# print "Hello there, ", response
+
+birth_year = 1985
+current_year = 2015
+age = current_year - birth_year
+print "You are " + str(age) + " years old."
