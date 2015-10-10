97a72649787603c2cd8d7b89c2c04ee4130b6774
andrewchosen <andrewchosen@fullsail.edu>
2015-10-10 16:12:24 -0700

    Notes from video #2

--- Simple Login/simple-login/main.py
+++ Simple Login/simple-login/main.py
@@ -9,22 +9,32 @@ import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self): #function that starts everything. Initializing
-        page = '''
+        page_head = '''
 <!DOCTYPE html>
 <html>
     <head>
         <title>Simple Form</title>
     </head>
     <body>
+        '''
+        page_body = '''
         <form method="GET">
             <label>Name</label><input type="text" name="user" />
             <label>Email</label><input type="text" name="email" />
             <input type="submit" value="Submit" />
+        '''
+        page_close = '''
         </form>
     </body>
 </html>
         '''
-        self.response.write(page) # Print
+        if self.request.GET:
+            # store form info
+            user =  self.request.GET['user']
+            email =  self.request.GET['email']
+            self.response.write(page_head+user+' '+email+page_body+page_close)
+        else:
+            self.response.write(page_head+page_body+page_close)
 
 #don't touch
 app = webapp2.WSGIApplication([
