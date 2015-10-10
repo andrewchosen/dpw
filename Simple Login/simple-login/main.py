5589bd3aeca5194f51bf31534cfcad1bc6375f35
andrewchosen <andrewchosen@fullsail.edu>
2015-10-10 15:36:43 -0700

    Simple form setup
    From class video #1

--- Simple Login/simple-login/main.py
+++ Simple Login/simple-login/main.py
@@ -1,32 +1,30 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 '''
 Name: Andrew Lancaster
 Date: 10/6/2015
 Class: DPW
+Assignment: Simple Login
 '''
 
 import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self): #function that starts everything. Initializing
-        self.response.write('Hello world!')
-        #code goes here
+        page = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Simple Form</title>
+    </head>
+    <body>
+        <form method="GET">
+            <label>Name</label><input type="text" name="user" />
+            <label>Email</label><input type="text" name="email" />
+            <input type="submit" value="Submit" />
+        </form>
+    </body>
+</html>
+        '''
+        self.response.write(page) # Print
 
 #don't touch
 app = webapp2.WSGIApplication([
