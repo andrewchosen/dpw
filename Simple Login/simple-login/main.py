5c7459c64a8b366d2db1c00ee944a4166a081153
andrewchosen <andrewchosen@fullsail.edu>
2015-10-10 18:14:19 -0700

    Added all form elements

--- Simple Login/simple-login/main.py
+++ Simple Login/simple-login/main.py
@@ -10,14 +10,44 @@ import webapp2 #use the webapp2 library
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self): #function that starts everything. Initializing
         p = Page()
+
+        if self.request.GET:
+            # store form info
+            user =  self.request.GET['user']
+            email =  self.request.GET['email']
+            p.body = "<p>" + user + " " + email + "</p>"
+        else:
+            p.body = """
+        <form method="GET">
+                    <fieldset id="name">
+                        <label for="first_name">First Name</label><input type="text" name="first_name" />
+                        <label for="last_name">Last Name</label><input type="text" name="last_name" />
+                    </fieldset>
+                    <fieldset id="gender">
+                        <label for="male">Male</label><input type="radio" name="gender" value="male" />
+                        <label for="female">Female</label><input type="radio" name="gender" value="female" />
+                    </fieldset>
+                    <fieldset id="age">
+                        <label for="age">Age</label><input type="number" name="age" min="13" max="150" />
+                    </fieldset>
+                    <fieldset id="status">
+                        <label for="status">Relationship Status</label>
+                        <select name="status">
+                            <option value="undisclosed">Undisclosed</option>
+                            <option value="single">Single</option>
+                            <option value="married">Married</option>
+                            <option value="divorced">Divorced</option>
+                            <option value="complicated">It's Complicated</option>
+                        </select>
+                    </fieldset>
+                    <fieldset id="location">
+                        <label for="location">Location</label><input type="text" name="location" />
+                    </fieldset>
+                    <input type="submit" value="Submit" />
+        </form>
+        """
+
         self.response.write(p.print_out())
-        # if self.request.GET:
-        #     # store form info
-        #     user =  self.request.GET['user']
-        #     email =  self.request.GET['email']
-        #     pass
-        # else:
-        #     pass
 
 class Page(object):
     def __init__(self):
@@ -32,13 +62,7 @@ class Page(object):
     </head>
     <body>
         """
-        self.body = """
-        <form method="GET">
-                    <label>Name</label><input type="text" name="user" />
-                    <label>Email</label><input type="text" name="email" />
-                    <input type="submit" value="Submit" />
-        </form>
-        """
+        self.body = ""
         self.close = """
 </body>
 </html>
@@ -47,6 +71,15 @@ class Page(object):
         all =  self.head + self.body + self.close
         all = all.format(**locals())
         return all
+
+class NewUser(object):
+    def __init__(self):
+        self.first_name = "John"
+        self.last_name = "Doe"
+        self.gender = "N/A"
+        self.age = 0
+        self.status = "Undisclosed"
+        self.location = "Everywhere"
 
 #don't touch
 app = webapp2.WSGIApplication([
