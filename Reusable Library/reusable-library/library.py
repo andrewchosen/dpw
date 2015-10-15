a263408ab5436975cf7f633b40d858d7cc606e43
andrewchosen <andrewchosen@fullsail.edu>
2015-10-15 16:33:30 -0700

    Notes/work from video 3

--- Reusable Library/reusable-library/library.py
+++ Reusable Library/reusable-library/library.py
@@ -3,12 +3,20 @@ class FavoriteMovies(object):
         self.__movie_list = []
         pass
     #some way to add to array of movies
-    def addMovie(self, m):
+    def add_movie(self, m):
         self.__movie_list.append(m)
         print m.title
 
-        #generate a list of movies
-        #calculate time span between movies
+    #generate a list of movies
+    def compile_list(self):
+        output = ''
+        for movie in self.__movie_list:
+            output += 'Title: ' + movie.title + ' (' + str(movie.year) + ')<br />'
+        return output
+
+    #calculate time span between movies
+    def calc_time_span(self):
+        pass
 
 
 class MovieData(object): #Data Object
@@ -18,7 +26,7 @@ class MovieData(object): #Data Object
         self.director = ''
     @property
     def year(self):
-        pass
+        return self.__year
 
     @year.setter
     def year(self, y): #if date is invalid
--- Reusable Library/reusable-library/main.py
+++ Reusable Library/reusable-library/main.py
@@ -16,15 +16,16 @@ class MainHandler(webapp2.RequestHandler):
         md1.title = "The Princess Bride"
         md1.year = 1989 # calling function
         md1.director = "Rob Reiner"
-        lib.addMovie(md1)
+        lib.add_movie(md1)
 
         md2 = MovieData()
         md2.title = "Teenage Mutant Ninja Turtles"
         md2.year = 1990 # calling function
         md2.director = "Some Guy"
+        lib.add_movie(md2)
 
 
-
+        p.body = lib.compile_list()
         self.response.write(p.print_out())
 
 app = webapp2.WSGIApplication([
