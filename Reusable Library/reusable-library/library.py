f13290280260061f783feaf3933a07cc6e70245f
andrewchosen <andrewchosen@fullsail.edu>
2015-10-15 16:18:13 -0700

    Notes/work from video 2

--- Reusable Library/reusable-library/library.py
+++ Reusable Library/reusable-library/library.py
@@ -1,3 +1,16 @@
+class FavoriteMovies(object):
+    def __init__(self):
+        self.__movie_list = []
+        pass
+    #some way to add to array of movies
+    def addMovie(self, m):
+        self.__movie_list.append(m)
+        print m.title
+
+        #generate a list of movies
+        #calculate time span between movies
+
+
 class MovieData(object): #Data Object
     def __init__(self):
         self.title = ''
--- Reusable Library/reusable-library/main.py
+++ Reusable Library/reusable-library/main.py
@@ -1,8 +1,13 @@
 import webapp2
-from library import MovieData
+from library import MovieData, FavoriteMovies
+from pages import Page
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+
+        #page for class
+        p = Page()
+        lib = FavoriteMovies()
 
         #movie title
         #year movie was made
@@ -11,11 +16,16 @@ class MainHandler(webapp2.RequestHandler):
         md1.title = "The Princess Bride"
         md1.year = 1989 # calling function
         md1.director = "Rob Reiner"
+        lib.addMovie(md1)
 
         md2 = MovieData()
         md2.title = "Teenage Mutant Ninja Turtles"
         md2.year = 1990 # calling function
         md2.director = "Some Guy"
+
+
+
+        self.response.write(p.print_out())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
