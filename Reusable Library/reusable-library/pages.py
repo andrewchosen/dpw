ca5f82cb1db04d3b12764e4f3c6e1b9630502dd1
andrewchosen <andrewchosen@fullsail.edu>
2015-10-15 18:29:24 -0700

    Privatized Page attributes and switched to **locals format

--- Reusable Library/reusable-library/pages.py
+++ Reusable Library/reusable-library/pages.py
@@ -1,24 +1,39 @@
 class Page(object):
     def __init__(self):
         self.__title = "Welcome!"
-        self.css = "css/styles.css"
+        self.__css = "css/styles.css"
         self.__head = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Enter your information:</title>
-        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <title>{self.title}</title>
+        <link href="{self.css}" rel="stylesheet" type="text/css" />
     </head>
     <body>
         """
-
-        self.body = ""
-        self.__error = ''
+        self.__body = ""
         self.__close = """
     </body>
 </html>
         """
 
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def css(self):
+        return self.__css
+
+    @property
+    def body(self):
+        return self.__body
+
+    @body.setter
+    def body(self, content):
+        self.__body = content
+
     def print_out(self):
-        all = self.__head + self.body + self.__error + self.__close
+        all = self.__head + self.__body + self.__close
+        all = all.format(**locals())
         return all
