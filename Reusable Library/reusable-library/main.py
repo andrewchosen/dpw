a9a5e2c72fea6547ac76f0b0d45e79e2d805350f
andrewchosen <andrewchosen@fullsail.edu>
2015-10-16 10:11:28 -0700

    Added form

--- Reusable Library/reusable-library/main.py
+++ Reusable Library/reusable-library/main.py
@@ -23,10 +23,21 @@ class MainHandler(webapp2.RequestHandler):
         food_list.add_food(food2)
 
         if self.request.GET:
-            p.body = "We have parameters!"
-        else:
             p.body = food_list.create_list() + "<br />" + food_list.total_calories()
-
+        else:
+            p.body = """
+        <form method="GET">
+                <fieldset id="food1">
+                    <label for="food_name">Name</label><input type="text" name="food_name" />
+                    <label for="calories">Calories</label><input type="text" name="calories" />
+                    <label for="quantity">Quantity</label><input type="text" name="quantity" />
+                </fieldset>
+                <fieldset id="total">
+                    <label for="daily_calories">Daily Calorie Goal</label><input type="text" name="daily_calories" />
+                </fieldset>
+                <button type="submit">Calculate</button>
+        </form>
+            """
         self.response.write(p.print_out())
 
 app = webapp2.WSGIApplication([
