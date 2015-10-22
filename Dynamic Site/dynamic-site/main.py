db4c3b96ad2b89c26f32789bd350a75319cdf165
andrewchosen <andrewchosen@fullsail.edu>
2015-10-22 16:19:41 -0700

    Added pages.py and imported HomePage() to main.py

--- Dynamic Site/dynamic-site/main.py
+++ Dynamic Site/dynamic-site/main.py
@@ -1,10 +1,13 @@
 import webapp2
 from data import Data
+from pages import HomePage
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        #Create instance of Data object
         d = Data()
-
+        p = HomePage()
+        self.response.write(p.print_out())
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
---
+++ Dynamic Site/dynamic-site/pages.py
@@ -0,0 +1,39 @@
+class Page(object):
+    def __init__(self):
+        self._title = "My Favorite Marvel Characters"
+        self._css = "css/styles.css"
+        self._js = "js/script.js"
+        self._head = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{self._title}</title>
+        <link href="{self._css}" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h1>{self._title}</h1>
+            </div>
+        </header>
+        <section>
+            <div class="container">
+        """
+        self._body = ""
+        self._close = """
+            </div>
+        </section>
+        <script language="javascript" type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script language="javascript" type="text/javascript" src="{self._js}"></script>
+    </body>
+</html>
+        """
+
+    def print_out(self):
+        all = self._head + self._body + self._close
+        all = all.format(**locals())
+        return all
+
+class HomePage(Page):
+    def __init__(self):
+        super(HomePage, self).__init__()
